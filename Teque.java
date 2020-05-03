ab3c81fb1688b4e4ae3d6135073819cc8e5a4b12
Tim <64743416+timltj@users.noreply.github.com>
2020-05-04 01:30:37 +0800

    Update Teque.java

--- Teque.java
+++ Teque.java
@@ -9,105 +9,97 @@
  * Lim Tiang Jung, Timothy 
  */
 
-import java.text.*;
 import java.util.*;
 import java.io.*;
 
 public class Teque {
 
-	// create 2 hash maps to simulate the front and back of a queue
-    	private static HashMap<Integer, Integer> front = new HashMap<>();
-    	private static HashMap<Integer, Integer> back = new HashMap<>();
-    	static int frontMin = 0;
-    	static int frontMax = 0;
-    	static int backMin = 0;
-    	static int backMax = 0;
+    // create 2 hash maps to simulate the front and back of a queue
+    private static HashMap<Integer, Integer> front = new HashMap<>();
+    private static HashMap<Integer, Integer> back = new HashMap<>();
+    private static int frontMin = 0;
+    private static int frontMax = 0;
+    private static int backMin = 0;
+    private static int backMax = 0;
 
-    	public static void main(String[] args) throws Exception {
-		/* fast io */
-        	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        	PrintWriter pw = new PrintWriter(System.out);
+    public static void main(String[] args) throws Exception {
+        /* fast io */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
 
-        	int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-		/* main loop to read all user inputs */
-        	for (int i = 0; i < n; i++) {
+        /* main loop to read all user inputs */
+        for (int i = 0; i < n; i++) {
 
-			// split each input into command and value
-			StringTokenizer st = new StringTokenizer(br.readLine());
-                  	String cmd = st.nextToken();
-                    	int x = Integer.parseInt(st.nextToken());
+            // split each input into command and value
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            int x = Integer.parseInt(st.nextToken());
 
-			// execute command
-                	switch(cmd) {
-                    		case "push_front":
-                        		push_front(x);
-                        		break;
-                    		case "push_back":
-                        		push_back(x);
-                        		break;
-                    		case "push_middle":
-                        		push_middle(x);
-                        		break;
-				case "get":
-					pw.write(Integer.toString(get(x)) + '\n');
-					break;
-                	}  		
-        	}
-        	pw.flush();
-    	}
+            // execute command
+            switch(cmd) {
+                case "push_front":
+                    push_front(x);
+                    break;
+                case "push_back":
+                    push_back(x);
+                    break;
+                case "push_middle":
+                    push_middle(x);
+                    break;
+                case "get":
+                    pw.write(Integer.toString(get(x)) + '\n');
+                    break;
+            }       
+        }
+        pw.flush();
+    }
 
-	/* client methods */
-	
-    	// push_front 
-    	private static void push_front(int x) {
-        	frontMin -= 1;
-        	front.put(frontMin, x);
-        	balance();
-    	}
+    /* client methods */           
+    private static void push_front(int x) { // push_front 
+        frontMin -= 1;
+        front.put(frontMin, x);
+        balance();
+    }
+   
+    private static void push_back(int x) { // push_back
+        back.put(backMax, x);
+        backMax += 1;
+        balance();
+    }
 
-    	// push_back
-    	private static void push_back(int x) {
-        	back.put(backMax, x);
-       		backMax += 1;
-        	balance();
-    	}
+    private static void push_middle(int x) { // push_middle
+        if (front.size() == back.size()) {
+            backMin -= 1;
+            back.put(backMin, x);
+        } else {
+            front.put(frontMax, back.get(backMin));
+            frontMax += 1;
+            back.put(backMin, x);
+        }
+    }
+    
+    private static int get(int index) { // get
+        if (index < frontMax - frontMin) {
+            return front.get(frontMin + index);
+        }
+        index -= (frontMax - frontMin);
+        return back.get(backMin + index);
+    }
 
-    	// push_middle 
-    	private static void push_middle(int x) {
-        	if (front.size() == back.size()) {
-            		backMin -= 1;
-            		back.put(backMin, x);
-        	}
-        	else {
-            		front.put(frontMax, back.get(backMin));
-           	 	frontMax += 1;
-            		back.put(backMin, x);
-        	}
-    	}
-
-    	// get
-    	private static int get(int index) {
-        	if (index < frontMax - frontMin) {
-           		return front.get(frontMin + index);
-		}
-        	index -= (frontMax - frontMin);
-        	return back.get(backMin + index);
-    	}
-
-    	// balance size of front and back queues
-    	private static void balance() {
-        	if (front.size() > back.size()) {
-            		frontMax -= 1;
-            		backMin -= 1;
-            		back.put(backMin, front.get(frontMax));
-            		front.remove(frontMax);
-         	}
-        	if (back.size() > front.size()+1) {
-            		front.put(frontMax, back.get(backMin));
-            		frontMax += 1;
-            		backMin += 1;
-            		back.remove(backMin-1);
-        	}
-    	}
+    // balance size of front and back queues
+    private static void balance() {
+        if (front.size() > back.size()) {
+            frontMax -= 1;
+            backMin -= 1;
+            back.put(backMin, front.get(frontMax));
+            front.remove(frontMax);
+        } else {
+            front.put(frontMax, back.get(backMin));
+            frontMax += 1;
+            backMin += 1;
+            back.remove(backMin-1);
+        }
+    }
 }
