5921441ff67291ea3651e7c290e0885bbc44aaaf
Tim <64743416+timltj@users.noreply.github.com>
2020-05-04 14:27:21 +0800

    Update SortOfSorting.java

--- SortOfSorting.java
+++ SortOfSorting.java
@@ -10,58 +10,52 @@
  * Lim Tiang Jung, Timothy 
  */
 
-import java.text.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class SortOfSorting {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		while (n != 0) {				
-			// store names
-			String[] arr = new String[n];
-			for (int x = 0; x < n; x++) {
-				arr[x] = sc.next();
-			}
+        int n = sc.nextInt();
+        while (n != 0) {                           
+            String[] arr = new String[n]; // store names
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.next();
+            }
 
-			bubbleSort(arr);
-			
-			for (int y = 0; y < n; y++) {
-				System.out.println(arr[y]);
-			}
+            bubbleSort(arr);
+            
+            for (int i = 0; i < n; i++) {
+                System.out.println(arr[i]);
+            }
+           
+            n = sc.nextInt(); // update n
+        }
+    }
 
-			// update n
-			n = sc.nextInt();
-		}
-	}
-
-	// bubble sort algorithm
-	public static void bubbleSort(String[] a) {
-		for (int i = 1; i < a.length; i++) {
-			boolean isSorted = true;
-			for (int j = 0; j < a.length - i; j++) {
-				// sort by first char
-				if ((int) a[j].charAt(0) > (int) a[j+1].charAt(0)) { 
-					String temp = a[j];
-					a[j] = a[j+1];
-					a[j+1] = temp;
-					isSorted = false;
-				}
-				// sort by second char
-				else if ((int) a[j].charAt(0) == (int) a[j+1].charAt(0)) { 
-					if ((int) a[j].charAt(1) > (int) a[j+1].charAt(1)) { 
-						String temp = a[j];
-						a[j] = a[j+1];
-						a[j+1] = temp;
-						isSorted = false;
-					}
-
-				}
-			}
-			if (isSorted) return;
-		}
-	}
-
+    /* bubble sort algorithm */
+    public static void bubbleSort(String[] a) {
+        for (int i = 1; i < a.length; i++) {
+            boolean isSorted = true;
+            for (int j = 0; j < a.length - i; j++) {                
+                if (a[j].charAt(0) > a[j+1].charAt(0)) { // sort by first char
+                    String temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    isSorted = false;
+                } else if (a[j].charAt(0) == a[j+1].charAt(0)) { // sort by second char
+                    if (a[j].charAt(1) > a[j+1].charAt(1)) { 
+                        String temp = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = temp;
+                        isSorted = false;
+                    }
+                }
+            }
+            if (isSorted) { 
+                return;
+            }
+        }
+    }
 }
