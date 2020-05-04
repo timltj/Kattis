c758e756dca114f3ddf6464be9be34f46e4f81e1
Tim <64743416+timltj@users.noreply.github.com>
2020-05-04 14:33:39 +0800

    Update Coconut.java

--- Coconut.java
+++ Coconut.java
@@ -9,83 +9,76 @@
  * Lim Tiang Jung, Timothy 
  */
 
-import java.text.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Coconut {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        	Scanner sc = new Scanner(System.in);
-		
-        	int s = sc.nextInt();
-        	int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        
+        int s = sc.nextInt();
+        int n = sc.nextInt();
 
-		// create a queue to store the hands
-        	Queue<Hand> queue = new LinkedList<>();
-		
-        	for (int i = 1; i < n + 1; i++) {
-            		queue.add(new Hand(i));
-            		queue.add(new Hand(i));
-        	}
+        // queue to store player hands
+        Queue<Hand> queue = new LinkedList<>();
+        
+        for (int i = 1; i < n + 1; i++) {
+            queue.add(new Hand(i));
+            queue.add(new Hand(i));
+        }
 
-		/* game simulation */
-        	boolean split = false; // to check for spliting into fists
-        	while (queue.size() > 1) {
-	        	int count = s;
+        /* game simulation */
+        boolean split = false; // to check for spliting into fists
+        while (queue.size() > 1) {
+            int count = s;
+                while(count > 1) { // continue until last person
+                    Hand hand = queue.peek();
+                    if (hand.state == 1) {
+                        queue.add(queue.remove());
+                        queue.add(queue.remove());
+                    } else {
+                        queue.add(queue.remove());
+                    }
+                   
+                    if (split) { // split, the next hand will have same state as previous
+                        queue.peek().updateState();
+                        split = false;
+                    }  
+                    
+                    count--;
+                }
 
-	    		// continue until last person
-           		while(count > 1) {
-				Hand hand = queue.peek();
+                // last person
+                Hand hand = queue.peek();
+                if (hand.state == 1) {
+                    hand.updateState();
+                    split = true;
+                } else if (hand.state == 2) {
+                    hand.updateState();
+                    queue.add(queue.remove());
+                } else {
+                    queue.remove();
+                }
+        }
 
-                		if (hand.state == 1) {
-		    			queue.add(queue.remove());
-                			queue.add(queue.remove());
-		                } 
-				else {
-                    			queue.add(queue.remove());
-                		}
-
-                		// if split, the next hand will have same state as previous 
-				if (split) {
-                    			queue.peek().updateState();
-                    			split = false;
-                		}
-	
-                		count--;
-            		}
-
-            		// last person
-           		Hand hand = queue.peek();
-		        if (hand.state == 1) {
-                		hand.updateState();
-		                split = true;
-            		}
-			else if (hand.state == 2) {
-		                hand.updateState();
-                		queue.add(queue.remove());
-		        } 
-			else {
-                		queue.remove();
-            		}
-        	}
-
-        	System.out.println(queue.peek().id);
-    	}
+        System.out.println(queue.peek().id);
+    }
 }
 
 class Hand {
-	// attributes
-    	int id;
-    	int state;
+    // attributes
+    protected final int id;
+    protected int state;
 
-	// constructor
-    	public Hand(int id) {
-        	this.id = id;
-	        this.state = 1;
-    	}
-	
-	// update the state of hand
-    	public void updateState() {
-        	this.state++;
-    	}
+    // constructor
+    public Hand(int id) {
+        this.id = id;
+        this.state = 1;
+    }
+        
+    public void updateState() {
+        this.state++;
+    }
 }
